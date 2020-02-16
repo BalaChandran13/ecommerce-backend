57b653ead47926c622fac161eb372d47c5ff28cf
Bala Chandran <chandrusbc@gmail.com>
2020-02-16 17:58:08 +0530

    change origin

--- src/main/java/com/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/controller/ProductController.java
@@ -21,7 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/product")
-@CrossOrigin(origins = "https://ecommerceproduct-frontend.herokuapp.com/")
+@CrossOrigin(origins = "http://ecommerceproduct-frontend.herokuapp.com/")
 @Slf4j
 public class ProductController {
 	
