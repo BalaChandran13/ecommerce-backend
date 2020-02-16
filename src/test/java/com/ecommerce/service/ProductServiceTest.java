8c69e5de77b594a1c644e3f6817b3fc8997d32c9
Bala Chandran <chandrusbc@gmail.com>
2020-02-16 21:52:10 +0530

    pom changes commit one2

--- src/test/java/com/ecommerce/service/ProductServiceTest.java
+++ src/test/java/com/ecommerce/service/ProductServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -15,7 +14,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.ecommerce.entity.Product;
 import com.ecommerce.repository.ProductRepository;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = ProductService.class)
 public class ProductServiceTest {
